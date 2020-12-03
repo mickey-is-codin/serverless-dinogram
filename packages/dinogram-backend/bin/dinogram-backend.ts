80bf555f40e082bcbfd71563ad2c787fc6300745
Mickey Smith <smithmick14@gmail.com>
2020-12-02 22:00:45 -0600

    scorched earth policy to get started

--- packages/dinogram-backend/bin/dinogram-backend.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { DinogramBackendStack } from '../lib/dinogram-backend-stack';
-
-const app = new cdk.App();
-new DinogramBackendStack(app, 'DinogramBackendStack');
