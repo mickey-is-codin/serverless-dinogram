f977eb978f959df25b3d5bc014f349da73cbad58
Mickey Smith <smithmick14@gmail.com>
2020-12-05 10:14:38 -0600

    Added Amplify SecretsManager logic

--- amplify/backend/function/dinogramapifunction/src/index.js
+++ amplify/backend/function/dinogramapifunction/src/index.js
@@ -1,14 +1,31 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
+const { AWS } = require('aws-sdk');
+
+const getSecretKey = async () => {
+  const secretsManager = new AWS.SecretsManager();
+  const secret = await secretsManager.getSecretValue({ 
+    SecretId: 'MICKEY_SECRET_KEY'
+  }).promise();
+  if (!secret) {
+    throw new Error('Secret not found');
+  }
+  return JSON.parse(secret.SecretString);
+};
 
 const typeDefs = gql`
   type Query {
     hello: String
+    betterHello: String
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (obj, params, context) => "Hello from Apollo",
+    hello: (obj, params, context) => `Hello from Apollo. Key`,
+    betterHello: async () => {
+      const secretKey = await getSecretKey;
+      return `Secret key is: ${secretKey}`;
+    }
   },
 };
 
