2ee42f81b9097fd1dd77f17666e9d90df099d6e1
Mickey Smith <smithmick14@gmail.com>
2020-12-09 22:19:33 -0600

    Beginning to make components for each delineation

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Navbar from './Navbar';
+
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 import { 
   toTimelineData,
+  toPresentTime,
   EARTH_AGE_HUNDRED_MILL,
 } from '../stratigraphy/geologicTimeline';
 
 import '../styles/tailwind.output.css';
 import '../styles/timeline.css';
+
+gsap.registerPlugin(ScrollTrigger);
 
 interface TimelineSectionProps {
   sectionNumber: number;
@@ -67,19 +74,75 @@ const TimelineBody = () => {
   );
 };
 
+const CurrentTime = ({ currentTime }: any) => {
+  const { eon, era, period, epoch } = currentTime;
+  const eonText = `Eon: ${eon}`;
+  const eraText = `Era: ${era}`;
+  const periodText = `Period: ${period}`;
+  const epochText = `Epoch: ${epoch}`;
+  return (
+    <div className="fixed text-left text-xl pt-10 px-10 left-10 text-bone">
+      <p>{eonText}</p>
+      <p>{eraText}</p>
+      <p>{periodText}</p>
+      <p>{epochText}</p>
+    </div>
+  );
+};
+
+// GSAP Basics
+// const Animation = () => {
+
+//   const boxRef: any = useRef();
+//   const timeline = gsap.timeline()
+//   useEffect(() => {
+//     timeline
+//       .to([boxRef.current], { x: '500px', duration: 1 })
+//       .to([boxRef.current], { x: '0', duration: 1 });
+//   });
+
+//   return (
+//     <div
+//       ref={boxRef}
+//       className="m-24 w-24 h-24 bg-red-300"
+//     >A</div>
+//   );
+// };
+
+const GeologicDelineation = ({ delineation }: any) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
 const Timeline = () => {
 
   const [ timelineData ] = useState(toTimelineData());
+  const { eons, eras, periods, epochs } = timelineData;
 
-  console.log('render');
+  const [ currentTime, setCurrentTime ] = useState(toPresentTime());
+
+  // Next step:
+  // Import years and place them on the timeline.
+  // Once that's done we can animate based on them
+  // Maybe literally make components for each
+  // Put components under the timeline body
 
   return (
     <div className="text-center">
       <Navbar />
+        <CurrentTime currentTime={currentTime} />
         <h1 className="text-3xl text-bone">
-          Dinosaurs Through the Ages
+          A Tour Through the Earth
         </h1>
         <TimelineStart />
+        {/* <Animation /> */}
+        <GeologicDelineation delineation={eons} />
+        <GeologicDelineation delineation={eras} />
+        <GeologicDelineation delineation={periods} />
+        <GeologicDelineation delineation={epochs} />
         <TimelineBody />
         <TimelineEnd />
     </div>
--- src/stratigraphy/geologicTimeline.ts
+++ src/stratigraphy/geologicTimeline.ts
@@ -1,27 +1,58 @@
 export const EARTH_AGE_HUNDRED_MILL = 460;
 
+export const toPresentTime = () => ({
+  eon: 'Phanerozoic',
+  era: 'Cenozoic',
+  period: 'Quaternary',
+  epoch: 'Holocene',
+});
+
+// start units = 10,000 years
 export const toTimelineData = () => {
   return {
     eons: [
-      {
-        name: 'phanerozoic',
-        startYearsAgo: 1000,
-        endYearsAgo: 0
-      }
+      { name: 'Phanerozoic', start: 54100 },
+      { name: 'Precambrian/Proterozoic', start: 250000 },
+      { name: 'Phanerozoic/Archean', start: 400000 },
+      { name: 'Phanerozoic/Hadean', start: 460000 },
     ],
     eras: [
-      {
-        name: 'caenozoic',
-        startYearsAgo: 500,
-        endYearsAgo: 0
-      }
+      { name: 'Cenozoic', start: 6600 },
+      { name: 'Mesozoic', start: 25190 },
+      { name: 'Paleozoic', start: 54100 },
+      { name: 'Neoproterozoic', start: 100000 },
+      { name: 'Mesoproterozoic', start: 160000 },
+      { name: 'Paleoproterozoic', start: 250000 },
+      { name: 'Neoarchean', start: 280000 },
+      { name: 'Mesoarchean', start: 320000 },
+      { name: 'Paleoarchean', start: 360000 },
+      { name: 'Eoarchean', start: 400000 },
+      { name: 'Earlier', start: 460000 },
     ],
     periods: [
-      {
-        name: 'quaternary',
-        startYearsAgo: 100,
-        endYearsAgo: 0
-      }
+      { name: 'Quaternary', start: 258 },
+      { name: 'Tertiary/Neogene', start: 23030 },
+      { name: 'Tertiary/Paleogene', start: 6600 },
+      { name: 'Cretaceous', start: 14500 },
+      { name: 'Jurassic', start: 20130 },
+      { name: 'Triassic', start: 25190 },
+      { name: 'Permian', start: 29890 },
+      { name: 'Carboniferous/Pennsylvanian', start: 32320 },
+      { name: 'Carboniferous/Mississippian', start: 35890 },
+      { name: 'Devonian', start: 41920 },
+      { name: 'Silurian', start: 44380 },
+      { name: 'Ordovician', start: 48540 },
+      { name: 'Cambrian', start: 54100 },
+      { name: 'Earlier', start: 460000 },
+    ],
+    epochs: [
+      { name: 'Holocene', start: 12 },
+      { name: 'Pleistocene', start: 258 },
+      { name: 'Pliocene', start: 533 },
+      { name: 'Miocene', start: 2303 },
+      { name: 'Oligocene', start: 3390 },
+      { name: 'Eocene', start: 5600 },
+      { name: 'Paleocene', start: 6600 },
     ],
   };
 };
