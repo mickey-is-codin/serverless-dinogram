a88dea45527d30a3d62236ebd95e8dc4bc086300
Mickey Smith <smithmick14@gmail.com>
2021-02-24 11:30:21 -0600

    No log

--- src/hooks/usePageTracking.ts
+++ src/hooks/usePageTracking.ts
@@ -5,7 +5,6 @@ import ReactGA from "react-ga";
 export const usePageTracking = () => {
   const location = useLocation();
   useEffect(() => {
-    console.log(location.pathname + location.search);
     ReactGA.initialize('UA-190526775-1');
     ReactGA.pageview(location.pathname + location.search);
   }, [location]);
