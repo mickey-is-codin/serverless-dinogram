b5d9ffa63ec59f5d231eac8e12f74f3fd899ebe1
Mickey Smith <smithmick14@gmail.com>
2021-02-25 21:54:01 -0600

    Removed test people

--- src/pages/people/index.ts
+++ src/pages/people/index.ts
@@ -7,24 +7,6 @@ export const routes: PageRoute[] = [
     route: 'eric-lund-dueling-dinosaurs',
     componentPath: 'EricLund',
   },
-  {
-    name: 'Person One',
-    description: 'This is person one interview',
-    route: 'person-one',
-    componentPath: 'PersonOne',
-  },
-  {
-    name: 'Person Two',
-    description: 'This is person two interview',
-    route: 'person-two',
-    componentPath: 'PersonTwo',
-  },
-  {
-    name: 'Person Three',
-    description: 'This is person three interview',
-    route: 'person-three',
-    componentPath: 'PersonThree',
-  },
 ];
 
 export default routes;
