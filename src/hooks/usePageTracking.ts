3611446b5a8c8316e8e94952691d3976da58074d
Mickey Smith <smithmick14@gmail.com>
2021-02-26 09:12:42 -0600

    Removed localhost analytics

--- src/hooks/usePageTracking.ts
+++ src/hooks/usePageTracking.ts
@@ -1,11 +1,29 @@
-import { useEffect} from "react";
+import { useState, useEffect} from "react";
 import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga";
 
+// export const usePageTracking = () => {
+//   const location = useLocation();
+//   useEffect(() => {
+//     ReactGA.initialize('UA-190526775-1');
+//     ReactGA.pageview(location.pathname + location.search);
+//   }, [location]);
+// };
+
 export const usePageTracking = () => {
   const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
   useEffect(() => {
-    ReactGA.initialize('UA-190526775-1');
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
+    if (!window.location.href.includes("localhost")) {
+      ReactGA.initialize('UA-190526775-1');
+    }
+    setInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.pageview(location.pathname + location.search);
+    }
+  }, [initialized, location]);
 };
